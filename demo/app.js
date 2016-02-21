852eb39cf158e4c2bd9f4031a0e2b57eaf52a24b
Łukasz Wątroba <lukasz.jan.watroba@gmail.com>
2016-02-21 18:22:11 +0100

    enable debug info

--- demo/app.js
+++ demo/app.js
@@ -4,10 +4,6 @@
   angular
     .module('myApp', [ 'ngAnimate', 'ngTouch', 'ngSanitize', 'vTabs' ])
 
-    .config(function ($compileProvider) {
-      $compileProvider.debugInfoEnabled(false);
-    })
-
     .controller('MainController', function ($scope) {
       $scope.verticalTabs = {
         active: 0
@@ -20,15 +16,16 @@
       $scope.pages = [
         {
           title: 'Tab 1',
-          content: '<img src="http://lorempixel.com/1000/500/nature/3" alt=""><blockquote>"Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem." <br>— Aliquam Libero</blockquote><ol><li>Lorem ipsum dolor sit amet</li><li>Nullam dignissim convallis est</li><li>Praesent mattis</li></ol><p>Lorem ipsum dolor sit amet, <em>consectetuer adipiscing elit</em></p>'
+          content: '<h4>Tab 1</h4><p>Lorem ipsum dolor sit amet, <em>consectetuer adipiscing elit</em></p><ol><li>Lorem ipsum dolor sit amet</li><li>Nullam dignissim convallis est</li><li>Praesent mattis</li></ol>'
         },
         {
           title: 'Tab 2',
-          content: '<img src="http://lorempixel.com/1000/500/nature/1" alt=""><blockquote>"Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem." <br>— Aliquam Libero</blockquote><ol><li>Lorem ipsum dolor sit amet</li><li>Nullam dignissim convallis est</li><li>Praesent mattis</li></ol><p>Lorem ipsum dolor sit amet, <em>consectetuer adipiscing elit</em></p>'
+          content: '<h4>Tab 2</h4><p>Lorem ipsum dolor sit amet, <em>consectetuer adipiscing elit</em></p><ol><li>Lorem ipsum dolor sit amet</li><li>Nullam dignissim convallis est</li><li>Praesent mattis</li></ol>'
         },
         {
-          title: 'Tab 3',
-          content: '<img src="http://lorempixel.com/1000/500/nature/4" alt=""><blockquote>"Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem." <br>— Aliquam Libero</blockquote><ol><li>Lorem ipsum dolor sit amet</li><li>Nullam dignissim convallis est</li><li>Praesent mattis</li></ol><p>Lorem ipsum dolor sit amet, <em>consectetuer adipiscing elit</em></p>'
+          title: 'Tab 3 (disabled)',
+          content: '<h4>Tab 3</h4><p>Lorem ipsum dolor sit amet, <em>consectetuer adipiscing elit</em></p><ol><li>Lorem ipsum dolor sit amet</li><li>Nullam dignissim convallis est</li><li>Praesent mattis</li></ol>',
+          isDisabled: true
         }
       ];
     });
